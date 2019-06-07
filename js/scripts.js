4c4fa4b22c787afc4a835663ff1d950b28d1b690
Jessica DiPonziano <jess@webdevjd.com>
2019-06-07 13:37:14 -0400

    Handle fetch requests and errors

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
 const usersUrl = 'https://randomuser.me/api/?results=12';
 const gallery = document.getElementById('gallery');
 const search = document.querySelector('.search-container');
+
+//Handle fetch requests
+async function getJSON(url) {
+  try {
+    const response = await fetch(url);
+    return await response.json();
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getEmployees(url) {
+  const employeeJSON = await getJSON(url);
+  return employeeJSON.results;
+}
